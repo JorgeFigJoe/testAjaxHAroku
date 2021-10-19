377c1b2eebb8cac71ce18d69bffbc7dac8a8d12a
Jorge Figueroa <50466247+JorgeFigJoe@users.noreply.github.com>
2021-10-18 20:54:31 -0500

    add podgress

--- registro.php
+++ registro.php
@@ -1,10 +1,25 @@
 <?php 
-	$conexion=mysqli_connect('localhost','root','','usuarios');
+/*	$conexion=mysqli_connect('localhost','root','','usuarios');
 
 	$email=$_POST['email'];
 	$password=$_POST['password'];
 
 	$sql="INSERT into usuarios (email,password)
 			values ('$email','$password')";
-	echo mysqli_query($conexion,$sql);
+	echo mysqli_query($conexion,$sql);*/
+
+
+	$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+               array(
+                'pdo.server' => array(
+                   'driver'   => 'pgsql',
+                   'email' => $dbopts["email"],
+                   'password' => $dbopts["pass"],
+                   'host' => $dbopts["host"],
+                   'port' => $dbopts["port"],
+                   'dbname' => ltrim($dbopts["path"],'/')
+                   )
+               )
+);
  ?>
